73199658a26543234b636465f419a0da7f53d9f3
kk10-x <khrithik1516@gmail.com>
2020-10-15 15:18:36 +0530

    lab4

--- areac.sh
+++
@@ -1,4 +0,0 @@
-echo "enter the radius"
-read r
-area=`echo 3.1415 \* $r \* $r|bc`
-echo $area
--- lab1.sh
+++
@@ -1,5 +0,0 @@
-
-echo 	"Today's date: `date`"
-echo 	"My shell: $SHELL"
-echo	"This month's calendar: `cal`"
-echo	"Today's date: `date "+%d%m%y"`"
